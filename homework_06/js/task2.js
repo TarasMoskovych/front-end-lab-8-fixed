5fc0d51053765b545821e4a7faee4e1e47ebb7a1
Taras Moskovych <moskovych.taras@gmail.com>
2018-02-18 14:22:30 +0200

    use camelCase naming

--- homework_06/js/task2.js
+++ homework_06/js/task2.js
@@ -4,15 +4,15 @@ var usd = Number(prompt("Input amount of USD:"));
 if(isNaN(eur) || isNaN(usd) || eur <= 0 || usd <= 0){
     console.log("Incorrect data");  
 } else{
-    var eur_uah_rate = 33.2324; // 10.02.18
-    var usd_uah_rate = 27.1240; // 10.02.18
+    var eurUahRate = 33.2324; // 10.02.18
+    var usdUahRate = 27.1240; // 10.02.18
         
-    var eur_uah_result = parseFloat((eur * eur_uah_rate).toFixed(2));
-    var usd_uah_result = parseFloat((usd * usd_uah_rate).toFixed(2));
-    var eur_usd_result = parseFloat((eur_uah_rate / usd_uah_rate).toFixed(2));
+    var eurUahResult = parseFloat((eur * eurUahRate).toFixed(2));
+    var usdUahResult = parseFloat((usd * usdUahRate).toFixed(2));
+    var eurUsdResult = parseFloat((eurUahRate / usdUahRate).toFixed(2));
         
-    var str_eur = (eur > 1) ? " euros are" : " euro is";
-    var str_usd = (usd > 1) ? " dollars are" : " dollar is";
+    var strEur = (eur > 1) ? " euros are" : " euro is";
+    var strUsd = (usd > 1) ? " dollars are" : " dollar is";
     
-    console.log(eur +  str_eur + " equal " + eur_uah_result + " UAH, " + usd +  str_usd + " equal " + usd_uah_result + " UAH, one euro is equal " + eur_usd_result + " dollars.");
+    console.log(eur +  strEur + " equal " + eurUahResult + " UAH, " + usd +  strUsd + " equal " + usdUahResult + " UAH, one euro is equal " + eurUsdResult + " dollars.");
 }
